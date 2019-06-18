39ea0d0ff8564dc03ce773acd012c3a70b78878b
adrianovirgilio <49097002+adrianovirgilio@users.noreply.github.com>
2019-06-18 20:34:44 -0300

    Add files via upload

--- cadastra.php
+++ cadastra.php
@@ -12,7 +12,17 @@
         //Criando conexão com o banco de dados através
         // da classe PDO
         try{
-            $con = PDO('mysql:host=localhost:3307;dbname=bd_aula1806',"root","usbw");
+            $con = new PDO('mysql:host=localhost:3307;dbname=bd_aula1806',"root","usbw");
+            $comando_sql = "INSERT INTO tb_clientes(nome,telefone)VALUES(?,?)";
+            $stmt = $con->prepare($comando_sql);
+            $stmt->bindParam(1,$nome);
+            $stmt->bindParam(2,$telefone);
+            $resultado = $stmt->execute();
+            if($resultado){
+                echo "Dados gravados com sucesso!";
+            }else{
+                echo "Erro ao tentar executar o comando SQL!";
+            }
         }catch(PDOException $e){
             echo "Erro ocorrido:" .$e->getMessage();
 
